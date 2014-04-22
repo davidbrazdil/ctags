84e58133a8523aafc28fb0878fe02f2efc830d03
David Brazdil <david.brazdil@gmail.com>
2014-04-22 23:07:37 +0000

    Added type field to the finishing NULL entry

--- extract_names.sh
+++ extract_names.sh
@@ -17,7 +17,7 @@ EOF
 
 while read FN_NAME; do
 	FN_SIGNATURE=`cat "$TMP_TAGS" | grep "^$FN_NAME	" | cut -f 5 | sed 's/signature://'`
-	FN_RETURNTYPE=`cat "$TMP_TAGS" | grep "^$FN_NAME	" | cut -f 6 | sed 's/returntype://' | sed 's/JNIEXPORT//' | sed 's/JNICALL//'`
+	FN_RETURNTYPE=`cat "$TMP_TAGS" | grep "^$FN_NAME	" | cut -f 6 | sed 's/returntype://' | sed 's/[ ]*JNIEXPORT[ ]*//' | sed 's/[ ]*JNICALL[ ]*//'`
 	echo "extern $FN_RETURNTYPE $FN_NAME $FN_SIGNATURE;" >> "$OUTPUT"
 done < "$TMP_NAMES"
 
@@ -26,17 +26,34 @@ cat <<EOF >> "$OUTPUT"
 typedef struct method_entry {
 	char *name;
 	void *func;
+	int type;
 } methodEntry;
 
 methodEntry cherijni_MethodList[] = {
 EOF
 
 while read FN_NAME; do
-	echo "	{ \"$FN_NAME\", &$FN_NAME }," >> "$OUTPUT"
+	FN_RETURNTYPE=`cat "$TMP_TAGS" | grep "^$FN_NAME	" | cut -f 6 | sed 's/returntype://' | sed 's/[ ]*JNIEXPORT[ ]*//' | sed 's/[ ]*JNICALL[ ]*//'`
+	case "$FN_RETURNTYPE" in
+	void) 	
+		FN_TYPE=1 
+		;;
+	jint|jlong|jbyte|jboolean|jchar|jshort|jfloat|jdouble|jsize)
+		FN_TYPE=2
+		;;
+	jobject|jclass|jstring|jthrowable|jweak|jarray|j*Array) 
+		FN_TYPE=3 
+		;;
+	*) 
+		echo "Unknown return type \"$FN_RETURNTYPE\""
+		exit 1
+		;;
+	esac
+	echo "	{ \"$FN_NAME\", &$FN_NAME, $FN_TYPE }," >> "$OUTPUT"
 done < "$TMP_NAMES"
 
 cat <<EOF >> "$OUTPUT"
-	{NULL, NULL}
+	{NULL, NULL, 0}
 };
 EOF
 
